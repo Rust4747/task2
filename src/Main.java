d22dcdc08d35845ab6f19bd76104b6cb392e65fb
Tony <yaraliev5@gmail.com>
2022-08-30 15:59:01 +0300

    домашнее задание 2

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+public class Main {
+    public static void main(String[] args) {
+        var dog=8;
+        System.out.println(dog);
+        dog=dog+4;
+        System.out.println(dog);
+        dog=dog-3;
+        System.out.println(dog);
+        var cat=3.6;
+        System.out.println(cat);
+        cat=cat+4;
+        System.out.println(cat);
+        cat=cat-1.6;
+        System.out.println(cat);
+        var paper=(763789);
+        System.out.println(paper);
+        paper=paper+4;
+        System.out.println(paper);
+        paper=paper-7639;
+        System.out.println(paper);
+        var friend=19;
+        System.out.println(friend);
+        friend=friend*10;
+        System.out.println(friend);
+        friend=friend/7;
+        System.out.println(friend);
+        var frog=3.5;
+        System.out.println(frog);
+        frog=frog*10;
+        System.out.println(frog);
+        frog=frog/3.5;
+        System.out.println(frog);
+        frog=frog+4;
+        System.out.println(frog);
+        var boxerWeightOne=78.2;
+        var boxerWeightTwo=82.7;
+        var totalWeight=boxerWeightOne+boxerWeightTwo;
+        System.out.println("общий вес "+totalWeight+" кг");
+        var weightDifference=boxerWeightTwo%boxerWeightOne;
+        System.out.println("разница в весе "+weightDifference+" кг");
+        var totalHours=640;
+        System.out.println(totalHours);
+        var oneEmployee=8;
+        System.out.println(oneEmployee);
+        var numberEmployees=totalHours/oneEmployee;
+        System.out.println("количество сотрудников "+numberEmployees);
+        numberEmployees=numberEmployees+94;
+        System.out.println(numberEmployees);
+        var quantityHours=totalHours/numberEmployees;
+        System.out.println("174 сотрудника должны работать "+quantityHours+" часа");
+    }
+}
